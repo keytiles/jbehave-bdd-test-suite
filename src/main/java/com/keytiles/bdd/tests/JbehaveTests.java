aa031f0242a79d1c93cf85016a6753557259358f
AttilaW <attila.wind@keytiles.com>
2025-04-28 08:32:32 +0200

    adding Python support

--- src/main/java/com/keytiles/bdd/tests/JbehaveTests.java
+++ src/main/java/com/keytiles/bdd/tests/JbehaveTests.java
@@ -1,5 +1,25 @@
 package com.keytiles.bdd.tests;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.io.CodeLocations;
+import org.jbehave.core.io.LoadFromRelativeFile;
+import org.jbehave.core.reporters.Format;
+import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.AbstractApplicationContext;
+
 import com.google.common.collect.Lists;
 import com.keytiles.bdd.plugins.MillisTimestampJBehaveTableValuePlugin;
 import com.sixt.tool.bdd_testsuite.CommonJBehaveTest;
@@ -11,27 +31,9 @@ import com.sixt.tool.bdd_testsuite.jbehave.plugin.ObjectStoreJBehaveTableValuePl
 import com.sixt.tool.bdd_testsuite.jbehave.steps.DockerSteps;
 import com.sixt.tool.bdd_testsuite.jbehave.steps.GoAppControllerSteps;
 import com.sixt.tool.bdd_testsuite.jbehave.steps.HttpSteps;
+import com.sixt.tool.bdd_testsuite.jbehave.steps.PythonAppControllerSteps;
 import com.sixt.tool.bdd_testsuite.jbehave.steps.WaitSteps;
 import com.sixt.tool.bdd_testsuite.jbehave.steps.WiremockSteps;
-import org.apache.commons.io.FileUtils;
-import org.jbehave.core.Embeddable;
-import org.jbehave.core.configuration.Configuration;
-import org.jbehave.core.embedder.EmbedderControls;
-import org.jbehave.core.io.CodeLocations;
-import org.jbehave.core.io.LoadFromRelativeFile;
-import org.jbehave.core.reporters.FilePrintStreamFactory.ResolveToPackagedName;
-import org.jbehave.core.reporters.StoryReporterBuilder;
-import org.jbehave.core.steps.InjectableStepsFactory;
-import org.jbehave.core.steps.InstanceStepsFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.jbehave.core.reporters.Format;
-
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.*;
 
 public class JbehaveTests extends CommonJBehaveTest {
 
@@ -45,10 +47,10 @@ public class JbehaveTests extends CommonJBehaveTest {
         File storiesFolder = TestsRunner.storiesFolder;
         String baseFolderPath = TestsRunner.testSetupFolder.getAbsolutePath();
         // we need all files in it recursively!
-        String[] extensions = {"story"};
+        String[] extensions = { "story" };
         Iterator<File> filesIterator = FileUtils.iterateFiles(storiesFolder, extensions, true);
         List<String> storyPaths = new ArrayList<>();
-        while(filesIterator.hasNext()) {
+        while (filesIterator.hasNext()) {
             File file = filesIterator.next();
             // we need a relative path to the stories folder
             String relPath = file.getAbsolutePath();
@@ -62,13 +64,13 @@ public class JbehaveTests extends CommonJBehaveTest {
     protected List<String> compositeStepsPaths() {
         List<String> compositeStepsPaths = new ArrayList<>();
         File compositeStepsFolder = TestsRunner.compositeStepsFolder;
-        if(compositeStepsFolder.exists()) {
+        if (compositeStepsFolder.exists()) {
             String baseFolderPath = TestsRunner.testSetupFolder.getAbsolutePath();
             // we need all files in it recursively!
-            String[] extensions = {"steps", "story"};
+            String[] extensions = { "steps", "story" };
             Iterator<File> filesIterator = FileUtils.iterateFiles(compositeStepsFolder, extensions, true);
 
-            while(filesIterator.hasNext()) {
+            while (filesIterator.hasNext()) {
                 File file = filesIterator.next();
                 // we need a relative path to the stories folder
                 String relPath = file.getAbsolutePath();
@@ -81,25 +83,31 @@ public class JbehaveTests extends CommonJBehaveTest {
 
     @Override
     protected void customizeConfiguration(Configuration configuration) {
-            // we remove complex reporting - keep only console and simple text reporting
-            StoryReporterBuilder reporterBuilder = configuration.storyReporterBuilder();
-            StoryReporterBuilder newReporterBuilder = new StoryReporterBuilder() //
-                    // Let's put the reports into "<testSetupFolder>/reports" folder
-                    // It's funny, that to achieve this we actually need to point this /reports/something because actually
-                    // it looks the (baked in) "jbehave" folder will be created as the sibling of the given path... :-P
-                    // so the below basically will encure that the reports will be in "<testSetupFolder>/reports/jbehave" ...
-                    .withCodeLocation(CodeLocations.codeLocationFromPath(TestsRunner.testSetupFolder.getAbsolutePath() +"/reports/something-does-not-matter")) //
-                    //.withCodeLocation(storiesFolderURL) //
-                    .withFailureTrace(reporterBuilder.reportFailureTrace()) //
-                    .withFailureTraceCompression(reporterBuilder.compressFailureTrace()) //
-                    .withCrossReference(reporterBuilder.crossReference());
-            newReporterBuilder.withFormats(Format.CONSOLE, Format.TXT);
-            configuration.useStoryReporterBuilder(newReporterBuilder);
+        // we remove complex reporting - keep only console and simple text reporting
+        StoryReporterBuilder reporterBuilder = configuration.storyReporterBuilder();
+        StoryReporterBuilder newReporterBuilder = new StoryReporterBuilder() //
+                // Let's put the reports into "<testSetupFolder>/reports" folder
+                // It's funny, that to achieve this we actually need to point this
+                // /reports/something because actually
+                // it looks the (baked in) "jbehave" folder will be created as the sibling of
+                // the given path... :-P
+                // so the below basically will encure that the reports will be in
+                // "<testSetupFolder>/reports/jbehave" ...
+                .withCodeLocation(CodeLocations.codeLocationFromPath(
+                        TestsRunner.testSetupFolder.getAbsolutePath() + "/reports/something-does-not-matter")) //
+                // .withCodeLocation(storiesFolderURL) //
+                .withFailureTrace(reporterBuilder.reportFailureTrace()) //
+                .withFailureTraceCompression(reporterBuilder.compressFailureTrace()) //
+                .withCrossReference(reporterBuilder.crossReference());
+        newReporterBuilder.withFormats(Format.CONSOLE, Format.TXT);
+        configuration.useStoryReporterBuilder(newReporterBuilder);
 
-            configuration //
-                    //.useStoryLoader(new LoadFromRelativeFile(TestsRunner.storiesFolder.toURI().toURL())) //
-                    .useStoryLoader(new LoadFromRelativeFile(CodeLocations.codeLocationFromPath(TestsRunner.testSetupFolder.getAbsolutePath()))) //
-                    .useCompositePaths(new HashSet<>(compositeStepsPaths())); //
+        configuration //
+                // .useStoryLoader(new
+                // LoadFromRelativeFile(TestsRunner.storiesFolder.toURI().toURL())) //
+                .useStoryLoader(new LoadFromRelativeFile(
+                        CodeLocations.codeLocationFromPath(TestsRunner.testSetupFolder.getAbsolutePath()))) //
+                .useCompositePaths(new HashSet<>(compositeStepsPaths())); //
 
     }
 
@@ -112,7 +120,8 @@ public class JbehaveTests extends CommonJBehaveTest {
         jBehaveTableValuePlugins.put("emptyString", new EmptyStringJBehaveTableValuePlugin());
         jBehaveTableValuePlugins.put("millisTS", new MillisTimestampJBehaveTableValuePlugin());
 
-        JBehaveTableValuePluginProvider pluginProvider = new JBehaveTableValuePluginProvider(jBehaveTableValuePlugins, testsApplicationContext);
+        JBehaveTableValuePluginProvider pluginProvider = new JBehaveTableValuePluginProvider(jBehaveTableValuePlugins,
+                testsApplicationContext);
         JBehaveUtil jbehaveUtil = new JBehaveUtil(testsApplicationContext, pluginProvider);
 
         InjectableStepsFactory stepsFactory = new InstanceStepsFactory(configuration(), Lists.newArrayList( //
@@ -120,8 +129,8 @@ public class JbehaveTests extends CommonJBehaveTest {
                 HttpSteps.createFromContext(testsApplicationContext, jbehaveUtil),
                 DockerSteps.createFromContext(testsApplicationContext, jbehaveUtil),
                 WaitSteps.createFromContext(testsApplicationContext, jbehaveUtil),
-                GoAppControllerSteps.createFromContext(testsApplicationContext, jbehaveUtil)
-        ));
+                GoAppControllerSteps.createFromContext(testsApplicationContext, jbehaveUtil),
+                PythonAppControllerSteps.createFromContext(testsApplicationContext, jbehaveUtil)));
         return stepsFactory;
     }
 
--- src/main/java/com/keytiles/bdd/tests/TestsRunner.java
+++ src/main/java/com/keytiles/bdd/tests/TestsRunner.java
@@ -1,6 +1,10 @@
 package com.keytiles.bdd.tests;
 
-import com.sixt.tool.bdd_testsuite.CommonJBehaveTest;
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import org.junit.internal.TextListener;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
@@ -9,10 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.AbstractApplicationContext;
 
-import java.io.File;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
+import com.sixt.tool.bdd_testsuite.CommonJBehaveTest;
 
 public class TestsRunner {
 
@@ -26,7 +27,8 @@ public class TestsRunner {
 
     private Result result;
 
-    public TestsRunner(AbstractApplicationContext springServiceContainerContext, File testSetupFolder, File storiesFolder, File compositeStepsFolder) {
+    public TestsRunner(AbstractApplicationContext springServiceContainerContext, File testSetupFolder,
+            File storiesFolder, File compositeStepsFolder) {
         CommonJBehaveTest.setTestsSpringContext(springServiceContainerContext);
         TestsRunner.springServiceContainerContext = springServiceContainerContext;
         TestsRunner.testSetupFolder = testSetupFolder;
@@ -37,30 +39,35 @@ public class TestsRunner {
          * attilaw - reconsidered this
          * As this is not possible with all class loaders for now let's do not do this!
          * 
-         * We have introduced the "fsfile://" url schema usable in .story files next to "file://" - so maybe we do not need this hack at all?
+         * We have introduced the "fsfile://" url schema usable in .story files next to
+         * "file://" - so maybe we do not need this hack at all?
          * 
-        // let's add the main folder to the classpath - this way files can be addressed with resource loader
-        addFolderToClassPath(testSetupFolder);
+         * // let's add the main folder to the classpath - this way files can be
+         * addressed with resource loader
+         * addFolderToClassPath(testSetupFolder);
          */
     }
 
     private void addFolderToClassPath(File folder) {
-            // source: https://gist.github.com/simonwoo/0338424508496b6e171e
-            try {
-                ClassLoader sysClassLoader = ClassLoader.getSystemClassLoader();
-                if(sysClassLoader instanceof URLClassLoader) {
-                    URLClassLoader urlClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-                    Class urlClass = URLClassLoader.class;
-                    Method method = urlClass.getDeclaredMethod("addURL", new Class[]{URL.class});
-                    method.setAccessible(true);
-                    method.invoke(urlClassLoader, new Object[]{folder.toURI().toURL()});
-                }else {
-                    LOG.warn("could not add tests folder '{}' to the classpath... system class loader is not the class type we support! class loader class: {}", folder, sysClassLoader.getClass());
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException("Oops! It looks we failed to add the tests folder '"+folder.getAbsolutePath()+"' to the classpath... error: "+e, e);
+        // source: https://gist.github.com/simonwoo/0338424508496b6e171e
+        try {
+            ClassLoader sysClassLoader = ClassLoader.getSystemClassLoader();
+            if (sysClassLoader instanceof URLClassLoader) {
+                URLClassLoader urlClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+                Class urlClass = URLClassLoader.class;
+                Method method = urlClass.getDeclaredMethod("addURL", new Class[] { URL.class });
+                method.setAccessible(true);
+                method.invoke(urlClassLoader, new Object[] { folder.toURI().toURL() });
+            } else {
+                LOG.warn(
+                        "could not add tests folder '{}' to the classpath... system class loader is not the class type we support! class loader class: {}",
+                        folder, sysClassLoader.getClass());
             }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Oops! It looks we failed to add the tests folder '" + folder.getAbsolutePath()
+                    + "' to the classpath... error: " + e, e);
+        }
     }
 
     public void runTests() {
@@ -68,8 +75,7 @@ public class TestsRunner {
         junit.addListener(new TextListener(System.out));
 
         Result result = junit.run(
-                JbehaveTests.class
-                );
+                JbehaveTests.class);
 
         resultReport(result);
     }
@@ -79,7 +85,8 @@ public class TestsRunner {
     }
 
     private void resultReport(Result result) {
-        LOG.info("Finished. Result: Failures: {}, Ignored: {}. Tests run: {}. Time: {} ms.", result.getFailureCount(), result.getIgnoreCount(), result.getRunCount(), result.getRunTime());
+        LOG.info("Finished. Result: Failures: {}, Ignored: {}. Tests run: {}. Time: {} ms.", result.getFailureCount(),
+                result.getIgnoreCount(), result.getRunCount(), result.getRunTime());
         int idx = 1;
         for (Failure failure : result.getFailures()) {
             LOG.error("failure #{}: {}", idx, failure);
